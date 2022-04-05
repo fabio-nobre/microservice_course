4de00df996b34d184b61976e450aa3ec5c2ed50b
fabio-nobre <f.oliveiranobre@gmail.com>
2022-04-05 09:23:47 -0300

    FetchType Eager/Lazy, @EntityGraph, FetchMode SELECT/SUBSELECT/JOIN, @Query e @Modify

--- src/main/java/com/ead/course/models/CourseModel.java
+++ src/main/java/com/ead/course/models/CourseModel.java
@@ -55,8 +55,31 @@ public class CourseModel implements Serializable {
     @Column(nullable = false)
     private UUID userInstructor;
 
+    /**
+     * - Relacionamentos
+     * - Um curso pode ter vários módulos
+     *
+     * - @OneToMany: Um curso para vários módulos
+     * - mappedBy = "course": Atributo dentro de ModuleModel que será Chave estrangeira
+     * - @JsonProperty: Tipo de acesso e serialização
+     *  -obs:
+     *      1-  Utilizando Set<> e não List<>:
+     *          Set: Não é ordenada e não permite duplicação
+     *          list: Ordenada e permite duplicação
+     *      2-  Várias associações/mapeamento dentro de uma unica entidade:
+     *          Quando em uma única consulta trazer todos esse relacionamentos ao utilizar
+     *          List<> o Hibernate só retornará a primeira lista ignorando as demais, para
+     *          corrigir isso utilizar o Set<>.
+     *      3-    Em operações de alteração quando utilizado o list<> o Hibernate poderá ter problemas perfomance
+     *      4-  Evitar utilizar relacionamento UNIDIRECIONAL, pois caso não seja explicitado o Hibernate irá realizar
+     *          de qualque forma gerando query e tabelas extras desnecessarias afetando a perfornance
+     *
+     *   fetch = FetchType.LAZY: Carregamento tardio. Executa consultas apenas se necessario.
+     *   @Fetch(FetchMode.SUBSELECT): Será realiza uma consulta para curso e outra para todos os modulos desse curso
+     */
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @OneToMany(mappedBy = "course", fetch = FetchType.LAZY )
+    @Fetch(FetchMode.SUBSELECT)
     private Set<ModuleModel> modules;
 
 }
--- src/main/java/com/ead/course/repositories/CourseRepository.java
+++ src/main/java/com/ead/course/repositories/CourseRepository.java
@@ -2,6 +2,7 @@ package com.ead.course.repositories;
 
 import com.ead.course.models.CourseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
@@ -9,5 +10,5 @@ import java.util.UUID;
  * JpaRepository<ModuleModel, UUID> - Extendendo JPA e informando a entidade
  * Extender JpaSpecificationExecutor para poder utilizar os Specs
  */
-public interface CourseRepository extends JpaRepository<CourseModel, UUID> {
+public interface CourseRepository extends JpaRepository<CourseModel, UUID>, JpaSpecificationExecutor<CourseModel> {
 }

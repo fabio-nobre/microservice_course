630a4b556174f6d30fe525a07c0de6d95110f591
fabio-nobre <f.oliveiranobre@gmail.com>
2022-06-06 09:54:29 -0300

    Iniciando com RabbitMQ e Spring AMQP

---
+++ src/main/java/com/ead/course/configs/RabbitmqConfig.java
@@ -0,0 +1,35 @@
+package com.ead.course.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+    // Conectar o microservice a instancia do RabbitMQ definido no aplication.yaml
+    // rabbitmq:
+    //    addresses: amqps://bygijfjz:dss9aHxJm43TdTU3omlJYVAzbWUK-pQC@stingray.rmq.cloudamqp.com/bygijfjz
+    @Autowired
+    CachingConnectionFactory cachingConnectionFactory;
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate template = new RabbitTemplate(cachingConnectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+}

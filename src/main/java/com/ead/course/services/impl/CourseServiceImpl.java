8557050c33dc9d48c80d7f7b1a2b1daa686cb817
fabio-nobre <f.oliveiranobre@gmail.com>
2022-07-06 16:45:11 -0300

    Delete Cascade com State Transfer Pattern

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -50,6 +50,7 @@ public class CourseServiceImpl implements CourseService {
             }
             moduleRepository.deleteAll(moduleModelList);
         }
+        courseRepository.deleteCourseUserByCourse(courseModel.getCourseId());
         courseRepository.delete(courseModel);
     }
 
